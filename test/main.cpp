3a732076b49f312ea1e64899317e4cadfc4ca7e0
zekZhang <zekunzhang@zekuns-MacBook-Pro.local>
2016-01-21 22:01:38 +0800

    add test code

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by zekun Zhang on 21/01/2016.
+//  Copyright © 2016 zekun Zhang. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+     cout<< "Game Over!" << endl;
+    
+    return 0;
+    
+}
